1fcac8f0f3f639acb3e877d0f83ad41e80b212ed
Nagma Bano <nagma.bano@sap.com>
2024-02-14 22:11:02 +0530

    added products and settings fragments

--- app/src/main/java/com/example/myrestaurantapp/ui/products/ProductsViewModel.kt
+++ app/src/main/java/com/example/myrestaurantapp/ui/products/ProductsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProductsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is products Fragment"
     }
     val text: LiveData<String> = _text
 }
