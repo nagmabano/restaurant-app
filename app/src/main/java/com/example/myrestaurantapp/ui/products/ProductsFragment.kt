973c999bcf5a068e37ae807389168f25ca681536
Nagma Bano <nagma.bano@sap.com>
2024-02-14 14:40:29 +0530

    name refactoring

--- app/src/main/java/com/example/myrestaurantapp/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/example/myrestaurantapp/ui/products/ProductsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myrestaurantapp.ui.dashboard
+package com.example.myrestaurantapp.ui.products
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.myrestaurantapp.databinding.FragmentDashboardBinding
+import com.example.myrestaurantapp.databinding.FragmentProductsBinding
 
-class DashboardFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentProductsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val productsViewModel =
+            ViewModelProvider(this).get(ProductsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        productsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
